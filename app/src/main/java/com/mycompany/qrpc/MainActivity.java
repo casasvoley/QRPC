205f2156640a3eccefaaf755890b4843521fc639
casasvoley <acasaszan@gmail.com>
2022-04-19 19:28:34 +0200

    Permissions

--- app/src/main/java/com/mycompany/qrpc/MainActivity.java
+++ app/src/main/java/com/mycompany/qrpc/MainActivity.java
@@ -2,9 +2,50 @@ package com.mycompany.qrpc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
 
+import com.google.android.gms.nearby.connection.Strategy;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String[] REQUIRED_PERMISSIONS;
+    static {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            REQUIRED_PERMISSIONS =
+                    new String[]{
+                            Manifest.permission.BLUETOOTH_SCAN,
+                            Manifest.permission.BLUETOOTH_ADVERTISE,
+                            Manifest.permission.BLUETOOTH_CONNECT,
+                            Manifest.permission.ACCESS_WIFI_STATE,
+                            Manifest.permission.CHANGE_WIFI_STATE,
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                    };
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            REQUIRED_PERMISSIONS =
+                    new String[]{
+                            Manifest.permission.BLUETOOTH,
+                            Manifest.permission.BLUETOOTH_ADMIN,
+                            Manifest.permission.ACCESS_WIFI_STATE,
+                            Manifest.permission.CHANGE_WIFI_STATE,
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                    };
+        } else {
+            REQUIRED_PERMISSIONS =
+                    new String[]{
+                            Manifest.permission.BLUETOOTH,
+                            Manifest.permission.BLUETOOTH_ADMIN,
+                            Manifest.permission.ACCESS_WIFI_STATE,
+                            Manifest.permission.CHANGE_WIFI_STATE,
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                    };
+        }
+    }
+
+    private static final Strategy STRATEGY = Strategy.P2P_STAR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
