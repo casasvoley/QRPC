d0a5416a17068df23d2f7b2d7eb59bfdf5321bb9
casasvoley <acasaszan@gmail.com>
2022-04-20 11:49:12 +0200

    Update MainActivity.java

--- app/src/main/java/com/mycompany/qrpc/MainActivity.java
+++ app/src/main/java/com/mycompany/qrpc/MainActivity.java
@@ -6,10 +6,12 @@ import android.Manifest;
 import android.os.Build;
 import android.os.Bundle;
 
+import com.google.android.gms.nearby.connection.ConnectionsClient;
 import com.google.android.gms.nearby.connection.Strategy;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Permisos requeridos en función de la versión de Android del dispositivo
     private static final String[] REQUIRED_PERMISSIONS;
     static {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -45,7 +47,48 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private static final Strategy STRATEGY = Strategy.P2P_STAR;
+    // Estartegia de conexión
+    private static final Strategy STRATEGY = Strategy.P2P_CLUSTER;
+
+    // Elección en el juego
+    private enum GameChoice {
+        ROCK,
+        PAPER,
+        SCISSORS;
+
+        boolean beats(GameChoice other) {
+            return (this == GameChoice.ROCK && other == GameChoice.SCISSORS)
+                    || (this == GameChoice.SCISSORS && other == GameChoice.PAPER)
+                    || (this == GameChoice.PAPER && other == GameChoice.ROCK);
+        }
+    }
+
+    // Cliente de conexión
+    private ConnectionsClient connectionsClient;
+
+    // Nombre código del jugador
+    private final String codeName = CodenameGenerator.generate();
+
+    // Datos del oponente
+    private String opponentEndpointId;
+    private String opponentName;
+    private int opponentScore;
+    private GameChoice opponentChoice;
+
+    // Datos del jugador
+    private int myScore;
+    private GameChoice myChoice;
+
+    private Button findOpponentButton;
+    private Button disconnectButton;
+    private Button rockButton;
+    private Button paperButton;
+    private Button scissorsButton;
+
+    // Textviews
+    private TextView opponentText;
+    private TextView statusText;
+    private TextView scoreText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
