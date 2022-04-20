f95247f2edd9c1f47750dc39b63ddf1acd98a0cd
casasvoley <acasaszan@gmail.com>
2022-04-20 12:00:31 +0200

    Update MainActivity.java

--- app/src/main/java/com/mycompany/qrpc/MainActivity.java
+++ app/src/main/java/com/mycompany/qrpc/MainActivity.java
@@ -2,16 +2,29 @@ package com.mycompany.qrpc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import android.Manifest;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.nearby.connection.ConnectionInfo;
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
 import com.google.android.gms.nearby.connection.ConnectionsClient;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
 import com.google.android.gms.nearby.connection.Strategy;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "QRPC";
 
     // Permisos requeridos en función de la versión de Android del dispositivo
     private static final String[] REQUIRED_PERMISSIONS;
@@ -92,6 +105,69 @@ public class MainActivity extends AppCompatActivity {
     private TextView opponentText;
     private TextView statusText;
     private TextView scoreText;
+
+    // Callbacks cuando se reciben payloads
+    private final PayloadCallback payloadCallback =
+            new PayloadCallback() {
+                @Override
+                public void onPayloadReceived(String endpointId, Payload payload) {
+                    opponentChoice = GameChoice.valueOf(new String(payload.asBytes(), UTF_8));
+                }
+
+                @Override
+                public void onPayloadTransferUpdate(String endpointId, PayloadTransferUpdate update) {
+                    if (update.getStatus() == PayloadTransferUpdate.Status.SUCCESS && myChoice != null && opponentChoice != null) {
+                        finishRound();
+                    }
+                }
+            };
+
+    // Callbacks cuando se encuentran otros dispositivos
+    private final EndpointDiscoveryCallback endpointDiscoveryCallback =
+            new EndpointDiscoveryCallback() {
+                @Override
+                public void onEndpointFound(String endpointId, DiscoveredEndpointInfo info) {
+                    Log.i(TAG, "onEndpointFound: endpoint found, connecting");
+                    connectionsClient.requestConnection(codeName, endpointId, connectionLifecycleCallback);
+                }
+
+                @Override
+                public void onEndpointLost(String endpointId) {}
+            };
+
+    // Callbacks cuando se conecta a otros dsipositivos
+    private final ConnectionLifecycleCallback connectionLifecycleCallback =
+            new ConnectionLifecycleCallback() {
+                @Override
+                public void onConnectionInitiated(String endpointId, ConnectionInfo connectionInfo) {
+                    Log.i(TAG, "onConnectionInitiated: accepting connection");
+                    connectionsClient.acceptConnection(endpointId, payloadCallback);
+                    opponentName = connectionInfo.getEndpointName();
+                }
+
+                @Override
+                public void onConnectionResult(String endpointId, ConnectionResolution result) {
+                    if (result.getStatus().isSuccess()) {
+                        Log.i(TAG, "onConnectionResult: connection successful");
+
+                        connectionsClient.stopDiscovery();
+                        connectionsClient.stopAdvertising();
+
+                        opponentEndpointId = endpointId;
+                        setOpponentName(opponentName);
+                        setStatusText(getString(R.string.status_connected));
+                        setButtonState(true);
+                    } else {
+                        Log.i(TAG, "onConnectionResult: connection failed");
+                    }
+                }
+
+                @Override
+                public void onDisconnected(String endpointId) {
+                    Log.i(TAG, "onDisconnected: disconnected from the opponent");
+                    resetGame();
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
