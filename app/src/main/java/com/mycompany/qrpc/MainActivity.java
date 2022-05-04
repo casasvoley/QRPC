ef614e1bada69670f80e1ec64a8176b4309a9f37
casasvoley <acasaszan@gmail.com>
2022-05-04 10:11:06 +0200

    Working on Android 5.0

--- app/src/main/java/com/mycompany/qrpc/MainActivity.java
+++ app/src/main/java/com/mycompany/qrpc/MainActivity.java
@@ -1,21 +1,32 @@
 package com.mycompany.qrpc;
 
+import androidx.annotation.CallSuper;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.AdvertisingOptions;
 import com.google.android.gms.nearby.connection.ConnectionInfo;
 import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
 import com.google.android.gms.nearby.connection.ConnectionResolution;
 import com.google.android.gms.nearby.connection.ConnectionsClient;
 import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.DiscoveryOptions;
 import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
 import com.google.android.gms.nearby.connection.Payload;
 import com.google.android.gms.nearby.connection.PayloadCallback;
@@ -62,7 +73,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // Estartegia de conexión
+    private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
+
+    // Estrategia de conexión
     private static final Strategy STRATEGY = Strategy.P2P_CLUSTER;
 
     // Elección en el juego
@@ -110,12 +123,12 @@ public class MainActivity extends AppCompatActivity {
     private final PayloadCallback payloadCallback =
             new PayloadCallback() {
                 @Override
-                public void onPayloadReceived(String endpointId, Payload payload) {
+                public void onPayloadReceived(@NonNull String endpointId, Payload payload) {
                     opponentChoice = GameChoice.valueOf(new String(payload.asBytes(), UTF_8));
                 }
 
                 @Override
-                public void onPayloadTransferUpdate(String endpointId, PayloadTransferUpdate update) {
+                public void onPayloadTransferUpdate(@NonNull String endpointId, PayloadTransferUpdate update) {
                     if (update.getStatus() == PayloadTransferUpdate.Status.SUCCESS && myChoice != null && opponentChoice != null) {
                         finishRound();
                     }
@@ -126,27 +139,27 @@ public class MainActivity extends AppCompatActivity {
     private final EndpointDiscoveryCallback endpointDiscoveryCallback =
             new EndpointDiscoveryCallback() {
                 @Override
-                public void onEndpointFound(String endpointId, DiscoveredEndpointInfo info) {
+                public void onEndpointFound(@NonNull String endpointId, @NonNull DiscoveredEndpointInfo info) {
                     Log.i(TAG, "onEndpointFound: endpoint found, connecting");
                     connectionsClient.requestConnection(codeName, endpointId, connectionLifecycleCallback);
                 }
 
                 @Override
-                public void onEndpointLost(String endpointId) {}
+                public void onEndpointLost(@NonNull String endpointId) {}
             };
 
     // Callbacks cuando se conecta a otros dsipositivos
     private final ConnectionLifecycleCallback connectionLifecycleCallback =
             new ConnectionLifecycleCallback() {
                 @Override
-                public void onConnectionInitiated(String endpointId, ConnectionInfo connectionInfo) {
+                public void onConnectionInitiated(@NonNull String endpointId, ConnectionInfo connectionInfo) {
                     Log.i(TAG, "onConnectionInitiated: accepting connection");
                     connectionsClient.acceptConnection(endpointId, payloadCallback);
                     opponentName = connectionInfo.getEndpointName();
                 }
 
                 @Override
-                public void onConnectionResult(String endpointId, ConnectionResolution result) {
+                public void onConnectionResult(@NonNull String endpointId, ConnectionResolution result) {
                     if (result.getStatus().isSuccess()) {
                         Log.i(TAG, "onConnectionResult: connection successful");
 
@@ -163,15 +176,207 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 @Override
-                public void onDisconnected(String endpointId) {
+                public void onDisconnected(@NonNull String endpointId) {
                     Log.i(TAG, "onDisconnected: disconnected from the opponent");
                     resetGame();
                 }
             };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
         setContentView(R.layout.activity_main);
+
+        findOpponentButton = findViewById(R.id.find_opponent);
+        disconnectButton = findViewById(R.id.disconnect);
+        rockButton = findViewById(R.id.rock);
+        paperButton = findViewById(R.id.paper);
+        scissorsButton = findViewById(R.id.scissors);
+
+        opponentText = findViewById(R.id.opponent_name);
+        statusText = findViewById(R.id.status);
+        scoreText = findViewById(R.id.score);
+
+        TextView nameView = findViewById(R.id.name);
+        nameView.setText(getString(R.string.codename, codeName));
+
+        connectionsClient = Nearby.getConnectionsClient(this);
+
+        resetGame();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (!hasPermissions(this, REQUIRED_PERMISSIONS)) {
+            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_REQUIRED_PERMISSIONS);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        connectionsClient.stopAllEndpoints();
+        resetGame();
+
+        super.onStop();
+    }
+
+    // Devuelve verdadero si se han concedido todos los permisos requeridos a la app
+    private static boolean hasPermissions(Context context, String... permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Gestiona la aceptación o no aceptación de los permisos requeridos
+    @CallSuper
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode != REQUEST_CODE_REQUIRED_PERMISSIONS) {
+            return;
+        }
+
+        int i = 0;
+        for (int grantResult : grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                Log.i(TAG, "Failed to request the permission " + permissions[i]);
+                Toast.makeText(this, R.string.error_missing_permissions, Toast.LENGTH_LONG).show();
+                finish();
+                return;
+            }
+            i++;
+        }
+        recreate();
+    }
+
+    // Encontrar oponente
+    public void findOpponent(View view) {
+        startAdvertising();
+        startDiscovery();
+        setStatusText(getString(R.string.status_searching));
+        findOpponentButton.setEnabled(false);
+    }
+
+    // Desconectarse del oponenete y reiniciar la GUI
+    public void disconnect(View view) {
+        connectionsClient.disconnectFromEndpoint(opponentEndpointId);
+        resetGame();
+    }
+
+    // Envía una elección {@link GameChoice} al otro jugador
+    public void makeMove(View view) {
+        if (view.getId() == R.id.rock) {
+            sendGameChoice(GameChoice.ROCK);
+        } else if (view.getId() == R.id.paper) {
+            sendGameChoice(GameChoice.PAPER);
+        } else if (view.getId() == R.id.scissors) {
+            sendGameChoice(GameChoice.SCISSORS);
+        }
+    }
+
+    // Iniciar descubrimiento de jugadores
+    private void startDiscovery() {
+        // Note: Discovery may fail. To keep this demo simple, we don't handle failures.
+        connectionsClient.startDiscovery(
+                getPackageName(), endpointDiscoveryCallback,
+                new DiscoveryOptions.Builder().setStrategy(STRATEGY).build());
+    }
+
+    // Iniciar anuncios de nuestra presencia
+    private void startAdvertising() {
+        // Note: Advertising may fail. To keep this demo simple, we don't handle failures.
+        connectionsClient.startAdvertising(
+                codeName, getPackageName(), connectionLifecycleCallback,
+                new AdvertisingOptions.Builder().setStrategy(STRATEGY).build());
+    }
+
+    // Elimina el estado del juego y actualiza la GUI en consecuencia
+    private void resetGame() {
+        opponentEndpointId = null;
+        opponentName = null;
+        opponentChoice = null;
+        opponentScore = 0;
+        myChoice = null;
+        myScore = 0;
+
+        setOpponentName(getString(R.string.no_opponent));
+        setStatusText(getString(R.string.status_disconnected));
+        updateScore(myScore, opponentScore);
+        setButtonState(false);
+    }
+
+    // Envía la selección del usuario (piedra, papel o tijeras) al otro jugador
+    private void sendGameChoice(GameChoice choice) {
+        myChoice = choice;
+        connectionsClient.sendPayload(
+                opponentEndpointId, Payload.fromBytes(choice.name().getBytes(UTF_8)));
+
+        setStatusText(getString(R.string.game_choice, choice.name()));
+        // No changing your mind!
+        setGameChoicesEnabled(false);
+    }
+
+    // Determina el vencedor de la ronda y actualiza la GUI y el estado del juego
+    private void finishRound() {
+        if (myChoice.beats(opponentChoice)) {
+            // Win!
+            setStatusText(getString(R.string.win_message, myChoice.name(), opponentChoice.name()));
+            myScore++;
+        } else if (myChoice == opponentChoice) {
+            // Tie, same choice by both players
+            setStatusText(getString(R.string.tie_message, myChoice.name()));
+        } else {
+            // Loss
+            setStatusText(getString(R.string.loss_message, myChoice.name(), opponentChoice.name()));
+            opponentScore++;
+        }
+
+        myChoice = null;
+        opponentChoice = null;
+
+        updateScore(myScore, opponentScore);
+
+        // Ready for another round
+        setGameChoicesEnabled(true);
+    }
+
+    // Activa y desactiva los botones de la GUI en función del estado de la conexión
+    private void setButtonState(boolean connected) {
+        findOpponentButton.setEnabled(true);
+        findOpponentButton.setVisibility(connected ? View.GONE : View.VISIBLE);
+        disconnectButton.setVisibility(connected ? View.VISIBLE : View.GONE);
+
+        setGameChoicesEnabled(connected);
+    }
+
+    // Activa y desactiva los botones de piedra, papel y tijeras
+    private void setGameChoicesEnabled(boolean enabled) {
+        rockButton.setEnabled(enabled);
+        paperButton.setEnabled(enabled);
+        scissorsButton.setEnabled(enabled);
+    }
+
+    // Muestra al usuario un mensaje de estado
+    private void setStatusText(String text) {
+        statusText.setText(text);
+    }
+
+    // Establece el nombre del oponente
+    private void setOpponentName(String opponentName) {
+        opponentText.setText(getString(R.string.opponent_name, opponentName));
+    }
+
+    // Actualiza el marcador
+    private void updateScore(int myScore, int opponentScore) {
+        scoreText.setText(getString(R.string.game_score, myScore, opponentScore));
     }
 }
